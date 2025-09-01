912f4a3f7fc21c1b6169fe65288ab5ba1f656b16
ttonightt <ttonightt@outlook.com>
2025-09-01 18:09:36 +0200

    Added UTF8 encoding to Byte.js

--- src/libs/Byte.js
+++ src/libs/Byte.js
@@ -50,12 +50,72 @@ export const UTF8 = {
 
 	charToCode (c) {
 
-		const blen = bitLength(c);
+		if ( c.length > 1 ) `c argument must be a string with length of 1! got: ${c.length}\nYou might try to pass the unicode character instead of UTF16 as well`;
 
+		const utf16 = c.charCodeAt(0);
+
+		const blen = bitLength(utf16);
+
+		if (blen <= 7) {
+
+			return [utf16];
+		}
+		if (blen <= 11) {
+
+			const codes = splitByBase(utf16, 6);
+
+			codes[0] += 0b11000000;
+			codes[1] += 0b10000000;
+
+			return codes;
+		}
+		if (blen <= 16) {
+
+			const codes = splitByBase(utf16, 6, 6);
+
+			codes[0] += 0b11100000;
+			codes[1] += 0b10000000;
+			codes[2] += 0b10000000;
+
+			return codes;
+		}
 	},
 
-	codeToChar () {
+	codeToChar (codes) {
 		
+		if ( codes.length === 1 ) {
+
+			if ( codes[0] & 0xc0 === 0 )
+
+				return String.fromCharCode( codes[0] );
+
+			throw `malformed utf8 code! got: ${b8(codes)}`;
+		}
+		if ( codes.length === 2 ) {
+
+			codes[0] &= 0xe0;
+			codes[1] &= 0xc0;
+
+			if ( codes[0] === 0xc0 && codes[1] === 0x80 )
+
+				return String.fromCharCode( (codes[0] << 6) + codes[1] );
+
+			throw `malformed utf8 code! got: ${b8(codes)}`;
+		}
+		if ( codes.length === 3 ) {
+
+			codes[0] &= 0xf0;
+			codes[1] &= 0xc0;
+			codes[1] &= 0xc0;
+
+			if ( codes[0] === 0xe0 && codes[1] === 0x80 && codes[2] === 0x80 )
+
+				return String.fromCharCode( (codes[0] << 12) + (codes[1] << 6) + codes[2] );
+
+			throw `malformed utf8 code! got: ${b8(codes)}`;
+		}
+
+		throw `malformed utf8 code! got: ${b8(codes)}`;
 	}
 };
 
