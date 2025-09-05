742683f1d592a85087d0ad8c3cf59a0ecea1ba33
ttonightt <ttonightt@outlook.com>
2025-09-05 11:46:11 +0200

    Added benchmark for different ways to concat arrays

--- benchmarks/basic.bench.js
+++ benchmarks/basic.bench.js
@@ -27,3 +27,44 @@ describe("Array of nums VS Int8Array", () => {
 
 	}, { time: 1000 });
 });
+
+describe.only("push + flat VS concat + flat VS push ... VS concat ...", () => {
+
+	const arr1 = new Array(1000).map( (v, i) => i );
+	const arr2 = new Array(100).map( () => 
+		new Array(
+			Math.round( Math.random() * 2 ) + 1
+		)
+		.map( (v, i) => i ) );
+
+	bench("push + flat", () => {
+	
+		for ( const item in arr2 )
+
+			arr1.push(item);
+
+		arr1.flat(1);
+
+	}, { time: 1000 });
+
+	bench("concat", () => {
+
+		arr1.concat(arr2);
+		arr1.flat(1);
+
+	}, { time: 1000 });
+
+	bench("push ...", () => {
+	
+		for ( const item in arr2 )
+
+			arr1.push(...item);
+
+	}, { time: 1000 });
+
+	bench("concat ...", () => {
+
+		arr1.concat(...arr2);
+
+	}, { time: 1000 });
+});
