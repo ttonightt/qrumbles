ee42528fd68397658f846eafe26524a65273241c
ttonightt <ttonightt@outlook.com>
2025-09-09 16:36:44 +0200

    Finished charblocks table output, Outlined an API of Charblocks, Working around cut()

--- src/Charblocks.js
+++ src/Charblocks.js
@@ -1,3 +1,4 @@
+import { BinaryAsArray } from "./libs/BinaryAsArray";
 
 class Charblocks {
 
@@ -9,9 +10,28 @@ class Charblocks {
 		};
 	}
 
-	constructor (sources) {
+	constructor (sources, bitLength) {
 
-		this.blocks = sources.map( src => Charblocks.create(src, false) );
+		this.bitLength = bitLength;
+
+		this.blocks = [];
+
+		let i = 0, left = this.bitLength;
+
+		while ( i < sources.length && left >= sources[i].bitLength ) {
+
+			this.blocks[i] = Charblocks.create( sources[i], false );
+
+			left -= sources[i].bitLength;
+			i++;
+		}
+
+		if ( left - sources[i].bitLength < 0) {
+
+			const [len] = sources[i].constructor.charsFitInto(left);
+
+			this.blocks[i] = Charblocks.create( sources[i].slice(0, len), false );
+		}
 	}
 
 	setSafety (i, bool) {
@@ -28,8 +48,28 @@ class Charblocks {
 
 	}
 
-	cut () {
+	cut (i) {
 
+		const b = this.blocks[i].data;
+		const b_ = this.blocks[i + 1].data;
+
+		const joined = BinaryAsArray.join( b, b_ );
+
+		const [chars, mod] = b_.constructor.charsFitInto( joined.bitLength );
+
+		const target = b_.constructor.fromBinary(joined);
+
+		target.validate().forEach( ({ blockIndex, bitLength }) => target.setInt(0, blockIndex * 11, bitLength) );
+
+		if (mod) {
+
+			this.blocks.splice( i + 1, 1, Charblocks.create( new BinaryAsArray(mod) ) );
+		} else {
+
+			this.blocks.splice( i + 1, 1 );
+		}
+
+		return this.blocks.splice( i, 1, Charblocks.create(target) );
 	}
 
 	moveStart () {
--- src/dev/CharblocksOutput.js
+++ src/dev/CharblocksOutput.js
@@ -1,77 +1,84 @@
 import seedrandom from "seedrandom";
 import Charblocks from "../Charblocks";
 import { Alphanum, AlphanumArray } from "../libs/Alphanum";
-import { Gen } from "../libs/beans";
+import { digits, Gen } from "../libs/beans";
+import { BinaryAsArray } from "../libs/BinaryAsArray";
 
 
-const random = seedrandom(1);
+const random = seedrandom(4);
 
 const charblocks = new Charblocks(
 	Gen(
 		Array,
 		20,
-		() => {
+		i => {
 
-			const length = Math.round( random() * 23 ) + 1;
+			if (i === 6) return new BinaryAsArray(6);
+
+			const length = Math.round( random() * 20 ) + 1;
 
 			const str = Gen( String, length, () => Alphanum.__ref[ Math.round(random() * (Alphanum.__ref.length - 1)) ] );
 
 			return new AlphanumArray(length).setStr( 0, str );
 		}
-	)
+	),
+	1000
 );
 
 charblocks.setSafety(3, true);
-charblocks.setSafety(10, true);
+charblocks.setSafety(7, true);
+charblocks.cut(2);
 
-const data = charblocks.blocks.map( cb => cb.data.type[0].toUpperCase() + cb.data.getStr().replace(/\s/g, "_") ).join("");
+const data = charblocks.blocks.map( ({data}) => data.type === "binary" ? "" : data.getStr().replace(/\s/g, "_") ).join("");
 
 const width = 20;
 let table = "";
 let c = 0;
 let sum = 0;
 
-for (let i = 0; i < data.length; i++) {
+for (let i = 0; i < data.length; i) {
 
-	if (i - c === sum && c < charblocks.blocks.length) {
+	if ((i + c) % width === 0 && i !== 0) {
+
+		table += "↲\n";
+	}
+
+	if (i === sum && c < charblocks.blocks.length) {
 
 		table += "\x1b[0m";
 
 		switch ( charblocks.blocks[c].data.type ) {
 			case "alphanumerical":
-				table += "\x1b[43m";
+				table += "\x1b[43mA";
 				break;
 			case "numerical":
-				table += "\x1b[32m";
+				table += "\x1b[32mN";
 				break;
 			case "byte":
-				table += "\x1b[36m";
+				table += "\x1b[36mB";
 				break;
 			case "binary":
-				table += "\x1b[37m";
+				table += "\x1b[41m\x1b[2m" + charblocks.blocks[c].data.bitLength;
 				break;
 			default:
 				throw `Unknown type!`;
 		}
 
-		table += data[i] + "\x1b[0m";
+		table += "\x1b[0m";
 
 		if ( !charblocks.blocks[c].safe ) {
 
 			table += "\x1b[2m";
 		}
 
-		sum += charblocks.blocks[c++].data.length;
+		sum += charblocks.blocks[c].data.type === "binary" ? digits(charblocks.blocks[c].data.bitLength, 10) - 1 : charblocks.blocks[c].data.length;
+		c++;
 
 	} else {
 
-		table += data[i];
-	}
-
-	if (i % width === width - 1) {
-
-		table += "\n";
+		table += data[i++];
 	}
 }
 
+//console.log( charblocks.blocks.map( ({data}) => data.type === "binary" ? "" : data.getStr() ) );
 console.log(table, "\x1b[0m");
